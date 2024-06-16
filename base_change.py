0359b7cc80e757be370b1fdd0bdd30fb59d9bfeb
changj <chang07@ads.uni-passau.de>
2024-06-16 15:02:11 -0400

    added prototype 'compression' code

--- base_change.py
+++ base_change.py
@@ -1,7 +1,8 @@
 import math
 import pandas
+import os
 
-path = "data_4_cloned"
+path = "data_0_cloned"
 for file in os.listdir(path):
     f = open(os.path.join(path,file), "r")
     # print("File being converted: ", file)
@@ -9,6 +10,15 @@ for file in os.listdir(path):
     s = f.read()
     f_data = [int(x) for x in s]
     #print(f_data)
-for i in f_data:
+c_data = []
+for i in range(0, len(f_data)):
+    # print(i)
+    if (i+1)%4 == 0:
+        print(i)
+        x = f_data[i-3]*(3**3) + f_data[i-2]*(3**2) + f_data[i-1]*(3) + f_data[i]
+        c_data.append(x)
+
+print(c_data)
+
     
 
